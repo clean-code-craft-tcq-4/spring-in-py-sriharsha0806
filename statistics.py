18dcc2321d3e39876cc3d316f6828e670a36925f
sriharsha annamaneni <sriharsha0806@gmail.com>
2022-05-09 16:38:24 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,16 +1,10 @@
 import math
 def calculateStats(nums):
   ans = {}
-  try:
-    ans["avg"] = sum(nums)/len(nums)
-  except:
-    ans["avg"] = math.nan
-   try:
-    ans["max"] = max(nums)
-   except:
-    ans["max"] = math.nan
-   try:
-    ans["min"] = min(nums)
-   except:
-    ans["min"] = math.none
+  try: ans["avg"] = sum(nums)/len(nums)
+  except: ans["avg"] = math.nan
+   try: ans["max"] = max(nums)
+   except: ans["max"] = math.nan
+   try: ans["min"] = min(nums)
+   except: ans["min"] = math.none
   return ans
