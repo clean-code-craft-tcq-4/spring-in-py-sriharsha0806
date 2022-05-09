228b8afb5c408d4506c4ff51f1b2604f679d3ffb
sriharsha annamaneni <sriharsha0806@gmail.com>
2022-05-09 16:25:19 +0530

    Updated the code with instructions in comments

--- statistics.test.py
+++ statistics.test.py
@@ -1,5 +1,6 @@
 import unittest
 import statistics
+import math 
 
 class StatsTest(unittest.TestCase):
   def test_report_min_max_avg(self):
@@ -8,6 +9,7 @@ class StatsTest(unittest.TestCase):
     self.assertAlmostEqual(computedStats["avg"], 4.525, delta=epsilon)
     self.assertAlmostEqual(computedStats["max"], 8.9, delta=epsilon)
     self.assertAlmostEqual(computedStats["min"], 1.5, delta=epsilon)
+    
 
   def test_avg_is_nan_for_empty_input(self):
     computedStats = statistics.calculateStats([])
@@ -15,6 +17,10 @@ class StatsTest(unittest.TestCase):
     # nan (not-a-number), as defined in the math package
     # Design the assert here.
     # Use nan and isnan in https://docs.python.org/3/library/math.html
+    self.assertAlmostEqual(computedStats["avg"], math.nan, delta=epsilon)
+    self.assertAlmostEqual(computedStats["max"], math.nan, delta=epsilon)
+    self.assertAlmostEqual(computedStats["min"], math.nan, delta=epsilon)
+    
 
   def test_raise_alerts_when_max_above_threshold(self):
     emailAlert = EmailAlert()
